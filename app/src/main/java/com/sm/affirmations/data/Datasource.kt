342c87ca4e0db7be860a98ea21930e14e174d064
Lixxxo <luis.rojas04@alumnos.ucn.cl>
2021-10-16 03:52:09 -0300

    feat(data/Datasource): set up the list of data

---
+++ app/src/main/java/com/sm/affirmations/data/Datasource.kt
@@ -0,0 +1,23 @@
+package com.sm.affirmations.data
+
+import com.sm.affirmations.R
+import com.sm.affirmations.model.Affirmation
+
+
+class Datasource {
+
+    fun loadAffirmations(): List<Affirmation> {
+        return listOf<Affirmation>(
+            Affirmation(R.string.affirmation1),
+            Affirmation(R.string.affirmation2),
+            Affirmation(R.string.affirmation3),
+            Affirmation(R.string.affirmation4),
+            Affirmation(R.string.affirmation5),
+            Affirmation(R.string.affirmation6),
+            Affirmation(R.string.affirmation7),
+            Affirmation(R.string.affirmation8),
+            Affirmation(R.string.affirmation9),
+            Affirmation(R.string.affirmation10)
+        );
+    }
+}
---
+++ app/src/main/java/com/sm/affirmations/model/Affirmation.kt
@@ -0,0 +1,3 @@
+package com.sm.affirmations.model
+
+data class Affirmation(val stringResourceId: Int)
